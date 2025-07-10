00537195297f8aeb5e5180a9897d81378d15b677
Daniel Koucher Machado <danielkm@post.com>
2025-07-10 15:47:12 -0300

    refactor the tokenizer

--- src/language/Tokenizer.zig
+++ src/language/Tokenizer.zig
@@ -7,6 +7,119 @@ const log = std.log.scoped(.usql_tokenizer);
 const This = @This();
 const Token = language.Token;
 
+const whitespace = " \t\r\n";
+const alphabetic = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const digit = "0123456789";
+const alphanumeric = alphabetic ++ digit;
+
+source: []const u8,
+index: usize = 0,
+
+pub fn init(source: []const u8) This {
+    return This{
+        .source = source,
+    };
+}
+
+fn next(self: *This) ?u8 {
+    if (self.index >= self.source.len) {
+        return null;
+    }
+    const c = self.source[self.index];
+    self.index += 1;
+    return c;
+}
+
+fn match(self: *This, chars: []const u8) bool {
+    if (self.index >= self.source.len) {
+        return false;
+    }
+    for (chars) |ch| {
+        if (self.source[self.index] == ch) {
+            self.index += 1;
+            return true;
+        }
+    }
+    return false;
+}
+
+fn peek(self: This) ?u8 {
+    if (self.index >= self.source.len) {
+        return null;
+    }
+    return self.source[self.index];
+}
+
+fn at(self: This, index: usize) ?u8 {
+    if (index >= self.source.len) {
+        return null;
+    }
+    return self.source[index];
+}
+
+fn slice(self: This, start: usize, end_offset: isize) []const u8 {
+    const end: usize = @intCast(@as(isize, @intCast(self.index)) + end_offset);
+    if (end >= self.source.len) {
+        return self.source[start..];
+    }
+    return self.source[start..end];
+}
+
+fn sliceForward(self: This, start_offset: isize, len: usize) []const u8 {
+    const start: usize = @intCast(@as(isize, @intCast(self.index)) + start_offset);
+    if (start + len >= self.source.len) return self.source[start..];
+    return self.source[start .. start + len];
+}
+
+pub fn token(self: *This) results.ParseResult(?Token) {
+    while (self.match(whitespace)) {}
+    if (self.peek() == null) {
+        return .OK(null);
+    }
+
+    const start = self.index;
+    if (self.match(alphabetic ++ "_")) { // identifiers and keywords
+        while (self.match(alphanumeric ++ "_")) {}
+        const word = self.slice(start, 0);
+        for (Token.keyword_list) |kw| {
+            if (std.ascii.eqlIgnoreCase(word, kw[0])) {
+                return .OK(Token.new(kw[1], word));
+            }
+        } else {
+            return .OK(Token.new(.{ .literal = word }, word));
+        }
+    } else if (self.match("\"'")) { // strings
+        while (self.next()) |c| {
+            if (c == self.at(start)) {
+                return .OK(Token.new(.{ .string = self.slice(start + 1, -1) }, self.slice(start, 0)));
+            }
+        } else {
+            return .ERR(.{ .never_closed_string = .{ .index = start } });
+        }
+    } else if (self.match(digit)) {
+        while (self.match(digit ++ ".")) {}
+        const number_literal = self.slice(start, 0);
+        const number = std.fmt.parseFloat(f32, number_literal) catch {
+            return .ERR(.{ .invalid_number = .{ .slice = number_literal } });
+        };
+        return .OK(Token.new(.{ .number = number }, number_literal));
+    } else { // operators
+        var best_match: ?@typeInfo(@TypeOf(Token.operator_list)).pointer.child = null;
+        for (Token.operator_list) |op| {
+            const sign = op[0];
+            if ((best_match == null or sign.len >= best_match.?[0].len) and std.mem.eql(u8, self.sliceForward(0, sign.len), sign)) {
+                best_match = op;
+            }
+        }
+        if (best_match) |operator| {
+            defer self.index += operator[0].len;
+            return .OK(Token.new(operator[1], self.sliceForward(0, operator[0].len)));
+        } else {
+            return .ERR(.{ .unexpected_character = .{ .character = &self.sliceForward(0, 1)[0] } });
+        }
+    }
+}
+
 /// Tokenizes the given expression into a slice of tokens.
 ///
 /// The slice is owned by the caller.
@@ -17,57 +130,11 @@ pub fn tokenize(expression: []const u8, allocator: std.mem.Allocator) !results.P
     var list = std.ArrayList(Token).init(allocator);
     defer list.deinit();
 
-    var i: usize = 0;
-    while (i < expression.len) : (i += 1) {
-        if (language.isWhitespace(expression[i])) { // Ignore whitespace
-            continue;
-        } else if (language.isAlphabetic(expression[i]) or expression[i] == '_') { // identifiers and keywords
-            const si = i;
-            while (i < expression.len and (language.isAlphanumeric(expression[i]) or expression[i] == '_')) : (i += 1) {}
-            const word = expression[si..i];
-            i -= 1;
-            for (Token.keyword_list) |kw| {
-                if (std.ascii.eqlIgnoreCase(word, kw[0])) {
-                    try list.append(Token.new(kw[1], word));
-                    break;
-                }
-            } else {
-                try list.append(Token.new(.{ .literal = word }, word));
-            }
-        } else if (expression[i] == '\'' or expression[i] == '"') { // strings
-            const si = i;
-            i += 1;
-            while (expression[i] != expression[si]) : (i += 1) {
-                if (i >= expression.len - 1) {
-                    return .ERR(.{ .never_closed_string = .{ .index = si } });
-                }
-            }
-            try list.append(Token.new(.{ .string = expression[si + 1 .. i] }, expression[si .. i + 1]));
-        } else if (language.isDigit(expression[i]) or (i + 1 < expression.len and expression[i] == '.' and language.isDigit(expression[i + 1]))) { // numbers
-            const si = i;
-            var has_dot = (expression[i] == '.');
-            while (i < expression.len and language.isDigit(expression[i]) or !has_dot and i + 1 < expression.len and expression[i] == '.' and language.isDigit(expression[i + 1])) : (i += 1) {
-                if (expression[i] == '.') has_dot = true;
-            }
-            const number_literal = expression[si..i];
-            const number = std.fmt.parseFloat(f32, number_literal) catch {
-                return .ERR(.{ .invalid_number = .{ .slice = number_literal } });
-            };
-            try list.append(Token.new(.{ .number = number }, number_literal));
-            i -= 1;
-        } else { // operators
-            var best_match: ?@typeInfo(@TypeOf(Token.operator_list)).pointer.child = null;
-            for (Token.operator_list) |op| {
-                const sign = op[0];
-                if ((best_match == null or sign.len >= best_match.?[0].len) and std.mem.eql(u8, expression[i .. i + sign.len], sign)) {
-                    best_match = op;
-                }
-            }
-            if (best_match) |operator| {
-                try list.append(Token.new(operator[1], expression[i .. i + operator[0].len]));
-            } else {
-                return .ERR(.{ .unexpected_character = .{ .character = &expression[i] } });
-            }
+    var tokenizer = This.init(expression);
+    while (true) {
+        switch (tokenizer.token()) {
+            .ok => |t| try list.append(t orelse break),
+            .err => |err| return .ERR(err),
         }
     }
 
