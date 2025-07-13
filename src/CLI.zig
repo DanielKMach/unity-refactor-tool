f047cddcde3319d807982cabc392ff785b4d9724
Daniel Koucher Machado <danielkm@post.com>
2025-07-13 17:10:37 -0300

    Update ansi

--- src/CLI.zig
+++ src/CLI.zig
@@ -13,13 +13,11 @@ pub const ExecutionMode = enum {
 };
 
 allocator: std.mem.Allocator,
-out: std.io.AnyWriter,
+out: std.fs.File.Writer,
 cwd: std.fs.Dir,
 
 pub fn process(self: This, args: *std.process.ArgIterator) !bool {
     var mode: ExecutionMode = .args;
-
-    ANSI.enable();
 
     if (args.next()) |arg| {
         if (std.mem.startsWith(u8, arg, "-")) {
@@ -94,7 +92,7 @@ pub fn process(self: This, args: *std.process.ArgIterator) !bool {
 }
 
 pub fn run(self: This, query: []const u8) !bool {
-    const result = try urt.eval(query, self.allocator, self.cwd, self.out);
+    const result = try urt.eval(query, self.allocator, self.cwd, self.out.any());
     switch (result) {
         .ok => return true,
         .err => |err| {
@@ -107,26 +105,22 @@ pub fn run(self: This, query: []const u8) !bool {
     }
 }
 
-pub fn printParseError(out: std.io.AnyWriter, errUnion: urt.results.ParseError, command: []const u8) !void {
+pub fn printParseError(out: std.fs.File.Writer, errUnion: urt.results.ParseError, command: []const u8) !void {
     const ansi = ANSI.init(out);
-    try ansi.red();
-    defer ansi.reset() catch {};
-
-    {
-        try ansi.bold();
-        defer ansi.unbold() catch {};
-        try out.writeAll("PARSING ERROR: ");
-    }
+    try ansi.print("*r", "PARSING ERROR: ", .{});
 
     switch (errUnion) {
         .unknown => {
-            try out.print("Unknown statement\r\n", .{});
+            try ansi.print("r", "Unknown statement\r\n", .{});
         },
         .never_closed_string => |err| {
-            try out.print("Never closed string at index {d}\r\n", .{err.index});
+            try ansi.print("r", "Never closed string at index {d}\r\n", .{err.index});
             try printLineHighlightRange(out, command, err.index, err.index);
         },
         .unexpected_token => |err| {
+            {
+                ansi.begin("r");
+                defer ansi.end("r");
             try out.print("Unexpected token '{s}'", .{@tagName(err.found.value)});
             if (err.expected.len > 0) try out.print(", expected ", .{});
             for (err.expected, 0..) |expected_type, i| {
@@ -135,22 +129,23 @@ pub fn printParseError(out: std.io.AnyWriter, errUnion: urt.results.ParseError, 
                 try out.print("{s}", .{@tagName(expected_type)});
             }
             try out.print("\r\n", .{});
+            }
             try printLineHighlight(out, command, err.found.lexeme);
         },
         .unexpected_character => |err| {
-            try out.print("Unexpected character '{c}'\r\n", .{err.character.*});
+            try ansi.print("r", "Unexpected character '{c}'\r\n", .{err.character.*});
             try printLineHighlight(out, command, err.character[0..1]);
         },
         .invalid_csharp_identifier => |err| {
-            try out.print("Invalid C# identifier '{s}'\r\n", .{err.identifier});
+            try ansi.print("r", "Invalid C# identifier '{s}'\r\n", .{err.identifier});
             try printLineHighlight(out, command, err.token.lexeme);
         },
         .invalid_guid => |err| {
-            try out.print("Invalid GUID '{s}'\r\n", .{err.guid});
+            try ansi.print("r", "Invalid GUID '{s}'\r\n", .{err.guid});
             try printLineHighlight(out, command, err.token.lexeme);
         },
         .invalid_number => |err| {
-            try out.print("Invalid number '{s}'\r\n", .{err.slice});
+            try ansi.print("r", "Invalid number '{s}'\r\n", .{err.slice});
             try printLineHighlight(out, command, err.slice);
         },
         .multiple => |errs| {
@@ -161,30 +156,23 @@ pub fn printParseError(out: std.io.AnyWriter, errUnion: urt.results.ParseError, 
     }
 }
 
-pub fn printRuntimeError(out: std.io.AnyWriter, errUnion: urt.results.RuntimeError) !void {
+pub fn printRuntimeError(out: std.fs.File.Writer, errUnion: urt.results.RuntimeError) !void {
     const ansi = ANSI.init(out);
-    try ansi.red();
-    defer ansi.reset() catch {};
-
-    {
-        try ansi.bold();
-        defer ansi.unbold() catch {};
-        try out.writeAll("RUNTIME ERROR: ");
-    }
+    try ansi.print("*r", "RUNTIME ERROR: ", .{});
 
     switch (errUnion) {
         .invalid_asset => |err| {
-            try out.print("Invalid asset path '{s}'\r\n", .{err.path});
+            try ansi.print("r", "Invalid asset path '{s}'\r\n", .{err.path});
         },
         .invalid_path => |err| {
-            try out.print("Invalid path '{s}'\r\n", .{err.path});
+            try ansi.print("r", "Invalid path '{s}'\r\n", .{err.path});
         },
     }
 }
 
 /// Shows a line with a highlight.
 /// Asserts that `highlight` is a slice of `source`.
-pub fn printLineHighlight(out: std.io.AnyWriter, source: []const u8, highlight: []const u8) !void {
+pub fn printLineHighlight(out: std.fs.File.Writer, source: []const u8, highlight: []const u8) !void {
     const zero = @intFromPtr(source.ptr);
     const offset = @intFromPtr(highlight.ptr) - zero;
     const len = if (highlight.len == 0) 1 else highlight.len;
@@ -192,7 +180,7 @@ pub fn printLineHighlight(out: std.io.AnyWriter, source: []const u8, highlight: 
     try printLineHighlightRange(out, source, offset, len);
 }
 
-pub fn printLineHighlightRange(out: std.io.AnyWriter, source: []const u8, offset: usize, size: usize) !void {
+pub fn printLineHighlightRange(out: std.fs.File.Writer, source: []const u8, offset: usize, size: usize) !void {
     const line_start = std.mem.lastIndexOf(u8, source[0..offset], "\n") orelse 0;
     const line_end = (std.mem.indexOf(u8, source[offset..], "\n") orelse source[offset..].len) + offset;
     const highlight_offset = offset - line_start;
@@ -201,17 +189,12 @@ pub fn printLineHighlightRange(out: std.io.AnyWriter, source: []const u8, offset
     const line = source[line_start..line_end];
 
     const ansi = ANSI.init(out);
-    defer ansi.reset() catch {};
-
-    try ansi.reset();
-    {
-        try ansi.bold();
-        defer ansi.unbold() catch {};
-        try out.print("On line {d}:\r\n", .{line_number});
-    }
+    try ansi.print("*", "On line {d}: \r\n", .{line_number});
     try out.print("{s}\r\n", .{line});
 
-    try ansi.green();
+    ansi.begin("g");
+    defer ansi.end("g");
+
     try out.writeByteNTimes(' ', highlight_offset);
     try out.writeByte('^');
     if (size > 1) try out.writeByteNTimes('~', size - 1);
@@ -220,7 +203,7 @@ pub fn printLineHighlightRange(out: std.io.AnyWriter, source: []const u8, offset
 }
 
 /// Prints the standard help message to the given writer.
-pub fn printHelp(out: std.io.AnyWriter) anyerror!void {
+pub fn printHelp(out: std.fs.File.Writer) anyerror!void {
     try out.writeAll(@embedFile("help.txt"));
 }
 
@@ -257,43 +240,61 @@ pub fn openURL(url: [:0]const u8) void {
 }
 
 pub const ANSI = struct {
-    out: std.io.AnyWriter,
+    enabled: bool = false,
+    out: std.fs.File.Writer,
 
-    pub fn init(out: std.io.AnyWriter) ANSI {
-        return ANSI{ .out = out };
-    }
-
-    pub fn bold(self: ANSI) !void {
-        try self.out.writeAll("\x1B[1m");
-    }
-    pub fn unbold(self: ANSI) !void {
-        try self.out.writeAll("\x1B[22m");
+    pub fn init(out: std.fs.File.Writer) ANSI {
+        var self = ANSI{ .out = out };
+        _ = self.enable();
+        return self;
     }
 
-    pub fn red(self: ANSI) !void {
-        try self.out.writeAll("\x1B[31m");
-    }
-    pub fn green(self: ANSI) !void {
-        try self.out.writeAll("\x1B[32m");
+    pub fn begin(self: ANSI, tags: []const u8) void {
+        if (!self.enabled) return;
+
+        for (tags) |tag| {
+            self.out.writeAll(switch (tag) {
+                '*' => "\x1B[1m",
+                '_' => "\x1B[4m",
+                'r' => "\x1B[31m",
+                'g' => "\x1B[32m",
+                'b' => "\x1B[34m",
+                'y' => "\x1B[33m",
+                'c' => "\x1B[36m",
+                'm' => "\x1B[35m",
+                else => continue,
+            }) catch continue;
+        }
     }
 
-    pub fn reset(self: ANSI) !void {
-        try self.out.writeAll("\x1B[0m");
+    pub fn end(self: ANSI, tags: []const u8) void {
+        if (!self.enabled) return;
+
+        for (tags) |tag| {
+            self.out.writeAll(switch (tag) {
+                '*' => "\x1B[22m",
+                '_' => "\x1B[24m",
+                'r', 'g', 'b', 'y', 'c', 'm' => "\x1B[39m",
+                else => continue,
+            }) catch continue;
+    }
+    }
+
+    pub fn print(self: ANSI, tags: []const u8, comptime format: []const u8, args: anytype) !void {
+        if (!self.enabled) {
+            try self.out.print(format, args);
+            return;
+    }
+
+        self.begin(tags);
+        defer self.end(tags);
+
+        try self.out.print(format, args);
     }
 
     /// Enables ANSI escape codes for the needed platforms (windows).
-    pub fn enable() void {
-        switch (builtin.os.tag) {
-            .windows => {
-                const api = @cImport(@cInclude("windows.h"));
-
-                const console = api.GetStdHandle(api.STD_OUTPUT_HANDLE);
-                var mode: api.DWORD = undefined;
-                if (api.GetConsoleMode(console, &mode) == 0) return;
-                mode |= api.ENABLE_VIRTUAL_TERMINAL_PROCESSING;
-                _ = api.SetConsoleMode(console, mode);
-            },
-            else => {},
-        }
+    pub fn enable(self: *ANSI) bool {
+        self.enabled = self.out.context.getOrEnableAnsiEscapeSupport();
+        return self.enabled;
     }
 };
