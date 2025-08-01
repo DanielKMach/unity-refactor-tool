0a7c00c67cc504170478e8010c743be541614d7d
Daniel Koucher Machado <danielkm@post.com>
2025-08-01 14:17:13 -0300

    add format functions

--- src/Token.zig
+++ src/Token.zig
@@ -57,10 +57,7 @@ pub fn is(self: Token, t: Type) bool {
     return self.value == t;
 }
 
-pub const Type = @typeInfo(Value).@"union".tag_type orelse unreachable;
-
-/// The type of tokens that can be recognized by the tokenizer.
-pub const Value = union(enum) {
+pub const Type = enum {
     // Keywords for statements
     SHOW,
     RENAME,
@@ -104,13 +101,85 @@ pub const Value = union(enum) {
     right_paren, // ')'
 
     /// A number literal.
-    number: f32,
+    number,
 
     /// A string literal.
-    string: []const u8,
+    string,
 
     /// Any alphanumeric literal, such as identifiers, component names, etc.
+    literal,
+
+    pub fn format(
+        self: Type,
+        comptime _: []const u8,
+        _: std.fmt.FormatOptions,
+        writer: anytype,
+    ) anyerror!void {
+        inline for (operator_list) |op| {
+            if (self == op[1]) {
+                return writer.print("'{s}'", .{op[0]});
+            }
+        }
+        inline for (keyword_list) |kw| {
+            if (self == kw[1]) {
+                return writer.print("'{s}'", .{kw[0]});
+            }
+        }
+        return writer.print("{s}", .{@tagName(self)});
+    }
+};
+
+/// The type of tokens that can be recognized by the tokenizer.
+pub const Value = union(Type) {
+    SHOW,
+    RENAME,
+    EVAL,
+    UPDATE,
+    OF,
+    IN,
+    WHERE,
+    AND,
+    OR,
+    NOT,
+    GUID,
+    DIRECT,
+    INDIRECT,
+    REFS,
+    USES,
+    FOR,
+    dot,
+    comma,
+    eos,
+    plus,
+    minus,
+    star,
+    slash,
+    equal,
+    greater,
+    greater_equal,
+    less,
+    less_equal,
+    bang_equal,
+    equal_equal,
+    left_paren,
+    right_paren,
+    number: f32,
+    string: []const u8,
     literal: []const u8,
+
+    pub fn format(
+        self: Value,
+        comptime _: []const u8,
+        _: std.fmt.FormatOptions,
+        writer: anytype,
+    ) anyerror!void {
+        switch (self) {
+            .number => |n| try writer.print("number '{d}'", .{n}),
+            .string => |s| try writer.print("string '{s}'", .{s}),
+            .literal => |l| try writer.print("literal '{s}'", .{l}),
+            else => try writer.print("{}", .{@as(Type, self)}),
+        }
+    }
 };
 
 pub const Location = struct {
