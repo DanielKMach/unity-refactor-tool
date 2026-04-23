31d7186c6931fe6846485d9648c4f25e23b66d8a
Daniel Koucher Machado <danielkm@post.com>
2026-04-23 00:19:12 -0300

    add guidOf func

--- lib/expr/value/Func.zig
+++ lib/expr/value/Func.zig
@@ -330,6 +330,19 @@ pub const builtin = struct {
         return .{ .string = try std.fmt.allocPrint(env.allocator, "{f}", .{id}) };
     }
 
+    pub fn guidOf(path: Value.Derived, env: Expr.eval.Env) Error!Value {
+        try Value.validate(path, &.{.string}, env.diag);
+        const id = blk: {
+            var buf: [std.fs.max_path_bytes]u8 = undefined;
+            const abspath = env.assets.proj.root.realpath(path.val.string, &buf) catch |e| break :blk e;
+            break :blk core.runtime.GUID.fromAsset(abspath, env.allocator);
+        } catch return env.err(.{ .invalid_asset = .{
+            .path = path.src.loc(),
+        } });
+
+        return .{ .string = try std.fmt.allocPrint(env.allocator, "{f}", .{id}) };
+    }
+
     pub fn assert(condition: Value.Derived, env: Expr.eval.Env) Error!Value {
         if (!condition.val.isTruthy()) return env.err(.{ .assertion_error = .{
             .desc = "The given value was false",
